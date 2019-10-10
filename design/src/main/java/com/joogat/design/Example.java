478b298771824b311b0c4ddbf290602952572bac
abdul759 <khiraabdulkadar@gmail.com>
2019-10-10 17:21:20 +0530

    Initial commit

---
+++ design/src/main/java/com/joogat/design/Example.java
@@ -0,0 +1,16 @@
+package com.joogat.design;
+
+
+public class Example {
+
+    private String design;
+
+    public Example(String design) {
+        this.design = design;
+    }
+
+
+    public String getDesign() {
+        return design;
+    }
+}
